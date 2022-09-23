0e409a18e9447c50dfd67b5e7416c6df6df287a1
Christine Parfitt <christineparfitt@MacBook-Pro.local>
2022-09-23 18:13:44 +0800

    practice add another if else to second py python py had some formatting changes

--- python.py
+++ python.py
@@ -1,9 +1,10 @@
 #we're finding out the first and last name of the user
 first = input('What is your first name?')
 last = input('and surname?')
-# we're trying to get this to have a number input
 
+# we're trying to get this to have a number input
 old = input('how old are you?')
+
 #change the input from a string to an integer
 age = int(old)
 print('Welcome', first, last, 'who is', age, 'years old')
--- second.py
+++ second.py
@@ -8,6 +8,10 @@ if x==1 :
     print('x equals 1')
 else :
     print('x not equal to 1')
-    
+
+if x<=3 :
+    print("less than or equal to three")
+else : print("greater than 3")
+
 
 print("all done")

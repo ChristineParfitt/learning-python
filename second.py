7c7d87516486fcff5d386a8d3b120b569006f8e2
Christine Parfitt <christineparfitt@MacBook-Pro.local>
2022-09-23 18:01:37 +0800

    added another if else to second py file

--- second.py
+++ second.py
@@ -1,8 +1,13 @@
 x = 1
 
 if x > 2 :
-    print('bigger')
+    print('x greater than 2')
 else :
-    print("smaller")
+    print("smaller than two")
+if x==1 :
+    print('x equals 1')
+else :
+    print('x not equal to 1')
+    
 
 print("all done")
